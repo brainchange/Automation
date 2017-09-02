4ce7f13b2a1ceab41ade92e740b383d9ad76ae7d
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-09-02 13:18:40 +0000

    Add files via upload

---
+++ udemy_html.sh
@@ -0,0 +1,114 @@
+nm=""
+eml=""
+pswd=""
+plink=""
+cno=""
+cmo=""
+cy=""
+cv=""
+current=""
+f=$(mktemp XXXXXXXXXX.html)
+echo "---name---|------email------|---password---|----profile--link-----|-------ccno-------|--ccmo--|--ccy--|-cvv-|----------current--courses-------|"
+INPUT=udemy_acc2.csv
+OLDIFS=$IFS
+IFS=";"
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+i=1
+while read name email password profile ccno ccmo ccy cvv courses
+do
+	echo "[$i]|$name|$email|$password|$profile|$ccno|$ccmo|$ccy|$cvv|$courses|"
+	i=$((i+1))
+done < $INPUT
+IFS=$OLDIFS
+read -p "Choose Account: " x
+i=1
+INPUT=udemy_acc2.csv
+OLDIFS=$IFS
+IFS=";"
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read name email password profile ccno ccmo ccy cvv courses
+do
+	if [ "$i" = "$x" ]; then
+		nm=$name
+		eml=$email
+		pswd=$password
+		plink=$profile
+		cno=$ccno
+		cmo=$ccmo
+		cy=$ccy
+		cv=$cvv
+		current=$courses
+		break
+	fi
+	i=$((i+1))
+done < $INPUT
+IFS=$OLDIFS
+read -p "How many courses to choose from main links and extra links? (example: 2 1) " a b
+IFS=";" read -a ADDR < udemy_main.csv
+IFS=";" read -a ADDR1 < udemy_extra.csv
+IFS=',' read -ra ADDR2 <<< "$current"
+curno=${#ADDR2[@]}
+curno=$((curno-1))
+count=1
+A=()
+while [ "$count" -le $a ]      # Generate 10 ($MAXCOUNT = $a) random integers.
+do
+  number=$RANDOM
+  let "number %= ${#ADDR[@]}"
+  initial=0
+  final=0
+  for i in `seq 0 $curno`;
+        do
+                if [[ "${ADDR[$number]}" = "${ADDR2[$i]}" ]]; then
+			echo "$nm is already enrolled in ${ADDR[$number]} trying other course!! "
+			initial=1
+			break
+		fi
+        done
+  if [ "$initial" = "$final" ]; then
+  	A+=(${ADDR[$number]})
+  	let "count += 1"  # Increment count.
+  fi
+done
+count=1
+B=()
+while [ "$count" -le $b ]      # Generate 10 ($MAXCOUNT =$b) random integers.
+do
+  number=$RANDOM
+  let "number %= ${#ADDR1[@]}"
+  initial=0
+  final=0
+  for i in `seq 0 $curno`;
+        do
+                if [[ "${ADDR1[$number]}" = "${ADDR2[$i]}" ]]; then
+			echo "$nm is already enrolled in ${ADDR1[$number]} trying other course!! "
+			initial=1
+			break
+		fi
+        done
+  if [ "$initial" = "$final" ]; then
+  	B+=(${ADDR1[$number]})
+  	let "count += 1"  # Increment count.
+  fi
+done
+echo "<!DOCTYPE html>" >> $f
+echo "<html>" >> $f
+echo "   <body>" >> $f
+echo "      <h1>Name: $nm <br>Email: $eml <br>Password: $pswd<br>Profile Link: $plink <br>CC No. : $cno <br>CC Expiry date: $cmo/$cy <br>CVV: $cv</h1>" >> $f
+echo "      <h1><br> MAIN LINKS <br></h1>" >> $f
+x=${#A[@]}
+no=$((x-1))
+for i in `seq 0 $no`;
+        do
+                echo "     <a href=${A[$i]}>${A[$i]}<br></a> " >> $f
+        done
+x=${#B[@]}
+no=$((x-1))
+echo "      <h1><br> EXTRA LINKS<br></h1>" >> $f
+for i in `seq 0 $no`;
+        do
+                echo "     <a href=${B[$i]}>${B[$i]}<br></a> " >> $f
+        done
+echo "   </body>" >> $f
+echo "</html>" >> $f
+google-chrome --no-sandbox /home/Automation/$f
--- udemy_scrapper.py
+++ udemy_scrapper.py
@@ -6,47 +6,47 @@ from bs4 import BeautifulSoup
 mainsite="https://www.udemy.com"
 with open('/home/Automation/udemy_acc1.csv') as csvin:
 	readfile=csv.reader(csvin, delimiter=";")
-	i=0
-	for row in readfile:
-		i=i+1
-		if i!=1:
-			print("Inside "+row[0]+"'s profile:")
-			print("Collecting Courses, please wait.. ")
-			courses=list()
-			url=row[3]
-			urls=list()
-			htmltext=urllib.urlopen(url).read()
-			soup= BeautifulSoup(htmltext,"lxml")
-			no=len(soup.find('ul',{'class': 'pagination'}).findAll('a'))
-			for i in range(1,no):
-				urls.append(url+'?subscribed_courses='+str(i)+'&key=subscribed_courses')
-			while len(urls)>0: #getting all the urls that are needed to be parsed
-				htmltext=urllib.urlopen(urls[0]).read()
+	with open('/home/Automation/udemy_acc2.csv','wb') as csvfile:
+		wr=csv.writer(csvfile,delimiter=';')
+		j=0
+		for row in readfile:
+			j=j+1
+			if j!=1:
+				print("Inside "+row[0]+"'s profile:")
+				print("Collecting Courses, please wait.. ")
+				courses=list()
+				firstcourse=""
+				currentcourse=""
+				url=row[3]
+				urls=list()
+				htmltext=urllib.urlopen(url).read()
 				soup= BeautifulSoup(htmltext,"lxml")
-				urls.pop(0)
-				for tag in soup.find('ul',{'class': 'course-cards-container'}).findAll('a',href= True):
-					tag['href'] = mainsite+tag['href']
-					print(tag['href'])
-					courses.append(tag['href'])
-				with open('/home/Automation/udemy_acc2.csv','wb') as csvfile:
-					wr=csv.writer(csvfile,delimiter=';')
-					wr.writerow([row[0]]+[row[1]]+[row[2]]+[row[3]]+[row[4]]+[row[5]]+[row[6]]+[row[7]]+[','.join(courses)])
-import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEImage import MIMEImage      
-try:
-	msg = MIMEMultipart()
-	msg.attach(MIMEText(file("/home/Automation/udemy_acc2.csv").read()))
-	
-
-	mail = smtplib.SMTP('smtp.gmail.com',587)
-
-	mail.ehlo()
-	mail.starttls()
-	mail.login("youremailid", "password")
-	mail.sendmail("youremailid@gmail.com", "targetemailid@gmail.com", msg.as_string())
-	mail.quit
-	print "Successfully sent email"
-except smtplib.SMTPException,error:
-	print str(error)
+				x=1
+				urls.append(url+'?subscribed_courses='+str(x)+'&key=subscribed_courses')
+				while len(urls)>0: #getting all the urls that are needed to be parsed
+					htmltext=urllib.urlopen(urls[0]).read()
+					soup= BeautifulSoup(htmltext,"lxml")
+					urls.pop(0)
+					if x==1:
+						for tag in soup.find('ul',{'class': 'course-cards-container'}).findAll('a',href= True):
+							firstcourse=tag['href']
+							break
+						for tag in soup.find('ul',{'class': 'course-cards-container'}).findAll('a',href= True):
+							tag['href'] = mainsite+tag['href']
+							print(tag['href'])
+							courses.append(tag['href'])
+						x=x+1
+						urls.append(url+'?subscribed_courses='+str(x)+'&key=subscribed_courses')
+					else:
+						for tag in soup.find('ul',{'class': 'course-cards-container'}).findAll('a',href= True):
+							currentcourse=tag['href']
+							break
+						if(firstcourse!=currentcourse):
+							for tag in soup.find('ul',{'class': 'course-cards-container'}).findAll('a',href= True):
+								tag['href'] = mainsite+tag['href']
+								print(tag['href'])
+								courses.append(tag['href'])
+							x=x+1
+							urls.append(url+'?subscribed_courses='+str(x)+'&key=subscribed_courses')
+				wr.writerow([row[0]]+[row[1]]+[row[2]]+[row[3]]+[row[4]]+[row[5]]+[row[6]]+[row[7]]+[','.join(courses)])
+os.system('mpack -s "Udemy CSV: current courses" /home/Automation/udemy_acc2.csv akshayg2014@email.iimcal.ac.in')
